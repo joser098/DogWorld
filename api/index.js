e439bd59464334f4c71e1a0c7306d5fe5da9830c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-05-30 13:30:23 -0300

    Deploy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
